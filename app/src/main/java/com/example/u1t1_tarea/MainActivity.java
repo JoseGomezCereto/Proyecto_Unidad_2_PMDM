57fe409e763fea27aadd5c6edea4a995dbfeb80a
JoseGomezCereto <trabajo.jgomez@gmail.com>
2023-10-17 21:31:40 +0200

    Añadida Toolbar con "añadir persona" y "borrar persona".
    Añade al Pequeño Timmy y lo borra satisfactoriamente.

--- app/src/main/java/com/example/u1t1_tarea/MainActivity.java
+++ app/src/main/java/com/example/u1t1_tarea/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.u1t1_tarea;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -25,12 +29,40 @@ public class MainActivity extends AppCompatActivity {
         personas.add(new Persona("Johnny Melavo", "32", R.drawable.foto2));
         personas.add(new Persona("Ching Chong Aloz", "22", R.drawable.foto3));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int idSeleccionado = item.getItemId();
+        if (idSeleccionado == R.id.menuNuevaPersona) {
+            Toast.makeText(this, "¡Hola, Pequeño Timmy!", Toast.LENGTH_SHORT).show();
+            personas.add(new Persona("Pequeño Timmy", "?", R.drawable.foto4));
+            adapter.notifyDataSetChanged();
+            return true;
+        } else if (idSeleccionado == R.id.menuBorrarPersona) {
+            Toast.makeText(this, "¡Nooo Pequeño Timmy!", Toast.LENGTH_SHORT).show();
+            personas.remove(3);
+            adapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private PersonasAdapter adapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         rvLista1 = findViewById(R.id.rvLista1);
         rvLista1.setHasFixedSize(true);
@@ -55,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 else
                     Toast.makeText(getApplicationContext(), "¿Y éste quien es?", Toast.LENGTH_SHORT).show();
             }
+
         });
 
         rvLista1.setAdapter(adapter);
